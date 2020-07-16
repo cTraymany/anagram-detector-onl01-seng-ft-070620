69b7323f8922b2a56f886ecdc561b61dd88105ed
Chindalath Traymany <chindalatht@gmail.com>
2020-07-16 12:05:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    matches = []
+    anagrams.each do |anagram|
+      letter = anagram.split("").sort
+      word.split.sort!
+      binding.pry
+      if letter == word
+        matches << anagram
+      end
+    end
+    matches
+  end
+
+
+end
